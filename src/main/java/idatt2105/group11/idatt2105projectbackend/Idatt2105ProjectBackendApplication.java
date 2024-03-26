cc45ebc24d6d252e2bf7f678ba8157aad7aac28a
Torbjørn Antonsen <torbjant@ntnu.no>
2024-03-26 11:28:26 +0100

    initial commit with spring initializr

---
+++ src/main/java/idatt2105/group11/idatt2105projectbackend/Idatt2105ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package idatt2105.group11.idatt2105projectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idatt2105ProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idatt2105ProjectBackendApplication.class, args);
+	}
+
+}
