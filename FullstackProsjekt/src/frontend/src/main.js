8f5078e75c1aac684eca8512318bbcf9a810e171
Maddy <113420588+MaddyPaddy2001@users.noreply.github.com>
2024-03-28 15:16:49 +0100

    Added more icons

--- FullstackProsjekt/src/frontend/src/main.js
+++ FullstackProsjekt/src/frontend/src/main.js
@@ -7,26 +7,9 @@ import "../src/assets/axios.js";
 import App from './App.vue'
 import router from './router'
 
-import '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/vue-fontawesome'
-import '@fortawesome/free-solid-svg-icons'
-
-
-/* import the fontawesome core */
-//import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-//import {faUser} from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-//library.add(faUser)
-
 
 const app = createApp(App)
 
-//app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
