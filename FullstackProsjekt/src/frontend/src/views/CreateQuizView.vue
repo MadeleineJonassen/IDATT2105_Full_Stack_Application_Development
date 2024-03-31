2f7a4feb4d60c074bbdaf213167c516adf861daf
Maddy <113420588+MaddyPaddy2001@users.noreply.github.com>
2024-03-31 16:40:04 +0200

    Created Modal for CreateQuizView.vue

--- FullstackProsjekt/src/frontend/src/views/CreateQuizView.vue
+++ FullstackProsjekt/src/frontend/src/views/CreateQuizView.vue
@@ -1,55 +1,106 @@
-<script>
+<script setup>
+import NewQuestionModel from "@/components/NewQuestionModel.vue";
+import {ref} from "vue";
+
+const showNewQuestionModal=ref(false)
+
+function createQuestion() {
+	showNewQuestionModal.value = true;
+}
+function destroyModal(){
+	showNewQuestionModal.value = false;
+}
 
 </script>
 
 
 <template>
 	<body>
-	<h1>Create a question to your quiz</h1>
-	<div class="question-table">
-		<table class="table">
-			<thead>
-			<tr>
-				<th scope="col">#</th>
-				<th scope="col">Question</th>
-				<th scope="col">Action</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr>
-				<th scope="row">1</th>
-				<td>What is Vue?</td>
-				<td>
-					<button> View</button>
-					<button> Edit</button>
-					<button> Delete</button>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row">2</th>
-				<td>Is this question 2?</td>
-				<td>
-					<button> View</button>
-					<button> Edit</button>
-					<button> Delete</button>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-	</div>
+		<div class="createQuestion-page">
+			<h1>Create a question to your quiz</h1>
+			<div class="question-table">
+				<table class="table">
+					<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Question</th>
+						<th scope="col">Action</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>What is Vue?</td>
+						<td>
+							<button class="play-btn">View</button>
+							<button class="edit-btn">Edit</button>
+							<button class="delete-btn"> Delete</button>
+						</td>
+					</tr>
 
-	<button> Create Question </button> <br>
-	<button> SAVE QUIZ </button>
+					</tbody>
+				</table>
 
+				<Teleport to="body">
+					<NewQuestionModel :show="showNewQuestionModal" @close="destroyModal">
+
+					</NewQuestionModel>
+				</Teleport>
+
+			</div>
+
+			<button @click="createQuestion" class="create-question-btn"> Create Question </button> <br>
+			<button class="save-question-btn"> SAVE QUIZ </button>
+		</div>
 	</body>
 
 </template>
 
 <style>
+.createQuestion-page{
+	padding: 20px;
+}
 .question-table{
 	align-items: stretch;
 	width: 100%;
 	background-color: #b0b0b0;
+}
+
+.play-btn{
+	height: 25px;
+	padding: 5px;
+	text-decoration: none;
+	background-color: #242F40;
+	border-color: transparent;
+	color: #FFFFFF;
+}
+.edit-btn{
+	height: 25px;
+	padding: 5px;
+	text-decoration: none;
+	background-color: #CCA43B;
+	border-color: transparent;
+	color: #242F40;
+}
+.delete-btn{
+	height: 25px;
+	padding: 5px;
+	text-decoration: none;
+	background-color: #cc513b;
+	border-color: transparent;
+	color: #242F40;
+}
+.create-question-btn{
+	padding: 20px;
+	margin: 10px;
+	background-color: #242F40;
+	color: #F2F2F2;
+}
+.save-question-btn{
+	padding: 20px;
+	margin: 10px;
+	background-color: #CCA43B;
 
 }
+
 </style>
--- FullstackProsjekt/src/frontend/src/views/OverviewQuizView.vue
+++ FullstackProsjekt/src/frontend/src/views/OverviewQuizView.vue
@@ -5,49 +5,55 @@
 
 <template>
 	<body>
+		<div class="overViewQuestion-page">
+		<h1>Your quizzes</h1>
+		<p> Select a quiz fo your creation to either play, edit or delete</p>
+			<div class="question-table">
+				<table class="table">
+					<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Quiz</th>
+						<th scope="col">Action</th>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Vue Quiz</td>
+						<td>
+							<router-link to="/playQuiz" class="play-btn">Play</router-link>
+							<router-link to="/createQuiz" class="edit-btn">Edit</router-link>
+							<button class="delete-btn"> Delete</button>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>What is Java?</td>
+						<td>
+							<router-link to="/playQuiz" class="play-btn">Play</router-link>
+							<router-link to="/overviewQuiz" class="edit-btn">Edit</router-link>
+							<button class="delete-btn"> Delete</button>
+						</td>
+					</tr>
 
-	<h1>Select your quiz</h1>
-	<div class="question-table">
-	<table class="table">
-		<thead>
-		<tr>
-			<th scope="col">#</th>
-			<th scope="col">Quiz</th>
-			<th scope="col">Action</th>
-		</tr>
-		</thead>
-		<tbody>
-		<tr>
-			<th scope="row">1</th>
-			<td>Vue Quiz</td>
-			<td>
-				<router-link to="/playQuiz" class="play-btn">Play</router-link>
-				<router-link to="/createQuiz" class="edit-btn">Edit</router-link>
-				<button class="delete-btn"> Delete</button>
-			</td>
-		</tr>
-		<tr>
-			<th scope="row">2</th>
-			<td>What is Java?</td>
-			<td>
-				<router-link to="/playQuiz" class="play-btn">Play</router-link>
-				<router-link to="/overviewQuiz" class="edit-btn">Edit</router-link>
-				<button class="delete-btn"> Delete</button>
-			</td>
-		</tr>
-		</tbody>
-	</table>
-	</div>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
 	</body>
 
 </template>
 
 <style>
+.overViewQuestion-page{
+	padding: 20px;
+}
+
 .question-table{
-	width: 500px;
-	background-color: #b0b0b0;
+	width: 450px;
 	padding: 10px;
-	margin: 20px;
 }
 
 .play-btn{
