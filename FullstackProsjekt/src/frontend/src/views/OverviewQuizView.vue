f128f5fba67821754cfacdb3802d2b9a07fa3f92
Maddy <113420588+MaddyPaddy2001@users.noreply.github.com>
2024-04-02 19:36:20 +0200

    Adjusted layout for OverviewQuizView.vue and add icons to category

--- FullstackProsjekt/src/frontend/src/views/OverviewQuizView.vue
+++ FullstackProsjekt/src/frontend/src/views/OverviewQuizView.vue
@@ -1,5 +1,11 @@
 <script>
 
+import {defineComponent} from "vue";
+import Svg from "@/assets/Svg.vue";
+
+export default defineComponent({
+	components: {Svg}
+})
 </script>
 
 
@@ -8,67 +14,41 @@
 		<div class="overViewQuestion-page">
 			<router-link to="/dashboard"> <- </router-link>
 		<h1>Your quizzes</h1>
-		<p> Select a quiz fo your creation to either play, edit or delete</p>
-			<div class="table">
-				<table class="table">
-					<thead>
-					<tr>
-						<th scope="col">#</th>
-						<th scope="col">Quiz</th>
-						<th scope="col">Action</th>
-						<th scope="col">Category </th>
-					</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Vue Quiz</td>
-						<td>
-							<router-link to="/play-quiz" class="play-btn">Play</router-link>
-							<router-link to="/createQuiz" class="edit-btn">Edit</router-link>
-							<button class="delete-btn"> Delete</button>
-						</td>
-						<th>
-							<div id="form-box">
-								<form>
-									<select id="language">
-										<option value="IT" selected="selected">IT</option>
-										<option value="Nature">Nature</option>
-										<option value="Chemistry">Chemistry</option>
-										<option value="Religion">Religion</option>
-										<option value="Other">Other</option>
-									</select>
-								</form>
-							</div>
-						</th>
-					</tr>
-					<tr>
-						<th scope="row">2</th>
-						<td>What is Java?</td>
-						<td>
-							<router-link to="/play-quiz" class="play-btn">Play</router-link>
-							<router-link to="/overviewQuiz" class="edit-btn">Edit</router-link>
-							<button class="delete-btn"> Delete</button>
-						</td>
-						<th>
-							<div id="form-box">
-								<form>
-									<select id="language">
-										<option value="IT" selected="selected">IT</option>
-										<option value="Nature">Nature</option>
-										<option value="Chemistry">Chemistry</option>
-										<option value="Religion">Religion</option>
-										<option value="Other">Other</option>
-									</select>
-								</form>
-							</div>
-						</th>
-					</tr>
-					</tbody>
-				</table>
+		<p> Select a quiz for your creation to either play, edit or delete</p>
+
+		<div class="row">
+			<div class="course-col">
+
+				<div class="quiz-header">
+				<h3>Quiz 1</h3>
+				<Svg name="cooking-category"/>
+				</div>
+
+				<div class="quiz-body">
+					<p> Info about quiz...</p>
+					<p>Category:</p>
+					<div id="form-box">
+						<form>
+							<select id="quiz-category">
+								<option value="IT" selected="selected">IT</option>
+								<option value="Nature">Nature</option>
+								<option value="Chemistry">Chemistry</option>
+								<option value="Religion">Religion</option>
+								<option value="Other">Other</option>
+							</select>
+						</form>
+					</div>
+				</div>
+
+				<div class="quiz-footer">
+					<router-link to="/play-quiz" class="play-btn">Play</router-link>
+					<router-link to="/createQuiz" class="edit-btn">Edit</router-link>
+					<button class="delete-btn"> Delete</button>
+				</div>
+
 			</div>
 		</div>
-
+		</div>
 	</body>
 
 </template>
@@ -76,5 +56,27 @@
 <style>
 .overViewQuestion-page{
 	padding: 20px;
+
+}
+.quiz-header{
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+.quiz-body{
+	margin-bottom: 10px;
+}
+#quiz-category{
+	padding: 1px;
+	background-color: #FFF;
+	border-radius: 5px;
+	font-family: monospace;
+}
+.quiz-footer{
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
 }
 </style>
