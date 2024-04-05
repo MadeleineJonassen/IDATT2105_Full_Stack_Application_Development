44e2959220c490d2c1f947fc4ecfefc591e8a992
Maddy <113420588+MaddyPaddy2001@users.noreply.github.com>
2024-04-05 13:59:50 +0200

    Added user profile in /profile

--- FullstackProsjekt/src/frontend/src/router/index.js
+++ FullstackProsjekt/src/frontend/src/router/index.js
@@ -58,6 +58,11 @@ const router = createRouter({
       component: EditQuizView,
       params: true
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue')
+    },
   ]
 })
 
