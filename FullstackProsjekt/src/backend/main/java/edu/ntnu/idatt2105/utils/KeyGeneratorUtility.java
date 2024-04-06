67143551eb155a4403c3f2b9ea9d95b6585ad0c1
Heine Mærde Brakstad <heinemb@stud.ntnu.no>
2024-04-06 13:55:48 +0200

    javadoc on all other files

--- FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/utils/KeyGeneratorUtility.java
+++ FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/utils/KeyGeneratorUtility.java
@@ -1,11 +1,18 @@
 package edu.ntnu.idatt2105.utils;
 
-
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
+/**
+ * Utility class for generating RSA keys.
+ */
 public class KeyGeneratorUtility {
 
+  /**
+   * Generates an RSA key pair.
+   *
+   * @return The generated RSA key pair.
+   */
   public static KeyPair generateRSAKey() {
     KeyPair keyPair;
 
@@ -14,7 +21,7 @@ public class KeyGeneratorUtility {
       keyPairGenerator.initialize(2048);
       keyPair = keyPairGenerator.generateKeyPair();
     } catch (Exception e) {
-      throw new IllegalStateException();
+      throw new IllegalStateException("Failed to generate RSA key pair.", e);
     }
 
     return keyPair;
