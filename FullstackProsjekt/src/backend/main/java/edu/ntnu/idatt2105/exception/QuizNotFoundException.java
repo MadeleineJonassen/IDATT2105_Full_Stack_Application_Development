09777e55408738dede7c329b707686401bc0306f
Heine Mærde Brakstad <heinemb@stud.ntnu.no>
2024-04-06 12:32:52 +0200

    javadoc on all Exception files

--- FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/QuizNotFoundException.java
+++ FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/QuizNotFoundException.java
@@ -1,7 +1,15 @@
 package edu.ntnu.idatt2105.exception;
 
-
+/**
+ * Exception indicating that a quiz was not found.
+ */
 public class QuizNotFoundException extends RuntimeException {
+
+  /**
+   * Constructor for QuizNotFoundException.
+   *
+   * @param message The exception message.
+   */
   public QuizNotFoundException(String message) {
     super(message);
   }
