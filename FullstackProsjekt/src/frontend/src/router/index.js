05ee30d3894c9825e956c9ee407e078ea3657273
Maddy <113420588+MaddyPaddy2001@users.noreply.github.com>
2024-04-06 12:39:50 +0200

    Fix new quiz bug, page is now visible

--- FullstackProsjekt/src/frontend/src/router/index.js
+++ FullstackProsjekt/src/frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import EditQuizView from "@/views/EditQuizView.vue";
 import PlayQuizView from "@/views/PlayQuizView.vue";
+import EditQuizView from "@/components/shared/create-quiz/EditQuizView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
