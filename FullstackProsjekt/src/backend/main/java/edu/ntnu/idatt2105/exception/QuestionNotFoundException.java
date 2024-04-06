581fef74e3306856743f4255cada3a89cb1aca55
Heine Mærde Brakstad <heinemb@stud.ntnu.no>
2024-04-06 12:32:52 +0200

    javadoc on all Exception files

--- FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/QuestionNotFoundException.java
+++ FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/QuestionNotFoundException.java
@@ -1,6 +1,15 @@
 package edu.ntnu.idatt2105.exception;
 
+/**
+ * Exception indicating that a question was not found.
+ */
 public class QuestionNotFoundException extends RuntimeException {
+
+  /**
+   * Constructor for QuestionNotFoundException.
+   *
+   * @param message The exception message.
+   */
   public QuestionNotFoundException(String message) {
     super(message);
   }
--- FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/UserNotFoundException.java
+++ FullstackProsjekt/src/backend/main/java/edu/ntnu/idatt2105/exception/UserNotFoundException.java
@@ -1,7 +1,15 @@
 package edu.ntnu.idatt2105.exception;
 
-
+/**
+ * Exception indicating that a user was not found.
+ */
 public class UserNotFoundException extends RuntimeException {
+
+  /**
+   * Constructor for UserNotFoundException.
+   *
+   * @param message The exception message.
+   */
   public UserNotFoundException(String message) {
     super(message);
   }
