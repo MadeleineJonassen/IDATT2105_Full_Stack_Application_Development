6958d8db7162c5e653c9d458d533a097eb31f0c6
Torbjørn Antonsen <torbjant@stud.ntnu.no>
2024-04-07 19:47:04 +0200

    Update index.js

--- FullstackProsjekt/src/frontend/src/router/index.js
+++ FullstackProsjekt/src/frontend/src/router/index.js
@@ -59,6 +59,11 @@ const router = createRouter({
       component: EditQuizView,
       params: true
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue')
+    }
   ]
 })
 
