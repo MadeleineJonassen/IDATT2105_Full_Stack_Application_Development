fc756f0a84e75e8935b3043e79bba585da3be392
krisskol <krisskol@stud.ntnu.no>
2024-04-08 09:44:52 +0200

    Remove token method

--- FullstackProsjekt/src/frontend/src/api.js
+++ FullstackProsjekt/src/frontend/src/api.js
@@ -5,15 +5,15 @@ export const apiClient = axios.create({
     baseURL: 'http://localhost:8080/api',
     //TODO: set api URL
 });
-
+/*
 apiClient.interceptors.request.use(
     (config) => {
         const token = getToken();
         if(token) {
             config.headers['Authorization'] = `Bearer ${token}`;
-        }
+        }else
         return config;
     },
     (error) => {
-    return Promise.reject("Problem with token: " + error);
-});
+    //return Promise.reject("Problem with token: " + error);
+});*/
